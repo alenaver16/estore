baec1d0d25a87b6c6fa445834a29ce945d9a22c0
alenaver16 <alenavereshaka16@gmail.com>
2019-04-25 22:02:45 +0300

    initial commit

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=estore',
+    //'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=estore',
+    'username' => 'root',
+    'password' => '12345',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
