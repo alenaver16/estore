e5c5cf1fe2381f636487504fee314e5153542d04
alenaver16 <alenavereshaka16@gmail.com>
2019-04-27 14:15:13 +0300

    basic settings for url

--- config/db.php
+++ config/db.php
@@ -3,7 +3,6 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=estore',
-    //'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=estore',
     'username' => 'root',
     'password' => '12345',
     'charset' => 'utf8',
